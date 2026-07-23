7f9947069e928181de10574b367bd939dd782370
yebow <yebow@JF-5000X-PC>
2026-07-23 12:13:40 -0700

    Add project files.

---
+++ Resume/Pages/About.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Resume.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ Resume/Pages/Projects.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Resume.Pages
+{
+    public class ProjectsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
